9f30e18700ce20886978796117838ef13f233610
Khadidja <khadija.aitsiali@gmail.com>
2022-05-19 20:55:14 +0200

    folder and files prepared

---
+++ src/components/Rate.js
@@ -0,0 +1,12 @@
+import React, {useState} from 'react';
+import star from '../assets/icon-star.svg';
+
+const Rate = () => {
+  return (
+    <div>
+      
+    </div>
+  );
+};
+
+export default Rate;
